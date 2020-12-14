225ec0e7f86bcf6cf50cac042f91302ec1a10dba
Minhyeong Joe <minhyeong.billy.joe@gmail.com>
2020-12-14 12:17:55 -0800

    added quicksort and binary search

--- Utils/sort.py
+++ Utils/sort.py
@@ -85,9 +85,70 @@ def mergeSort(arr):
 
 
 # quick sort
+def quickSort(arr):
+    def partition(arr, left, right):
+        pivotIndex = left
+        pivot = arr[right]
+        print("working on index {} to {}:".format(left, right))
+
+        for i in range(left, right):
+            print("i={}: comparing {} and {}".format(i, arr[i], pivot))
+            if arr[i] <= pivot:
+                arr[pivotIndex], arr[i] = arr[i], arr[pivotIndex]
+                pivotIndex += 1
+            print("{}, pivotIndex={}".format(arr, pivotIndex))
+        arr[pivotIndex], arr[right] = arr[right], arr[pivotIndex]
+        return pivotIndex
+
+    def sort(arr, left, right):
+        if left < right:
+            pivotIndex = partition(arr, left, right)
+            sort(arr, left, pivotIndex-1)
+            sort(arr, pivotIndex+1, right)
+
+    sort(arr, 0, len(arr)-1)
+
 
 # binary search (recursive)
+def binarySearch_rec(arr, target, left=0, right=None):
+    if right is None:
+        right = len(arr)-1
+    if left > right:
+        return -1
+    mid = (left+right)//2
+    if arr[mid] == target:
+        print("{} == {}".format(arr[mid], target))
+        return mid
+    elif arr[mid] > target:
+        print("{} > {}. will now search in {}".format(
+            arr[mid], target, arr[left:mid]))
+        return binarySearch_rec(arr, target, left, mid-1)
+    else:
+        print("{} < {}. will now search in {}".format(
+            arr[mid], target, arr[mid+1:right+1]))
+        return binarySearch_rec(arr, target, mid+1, right)
+
+
 # binary search (iterative)
+def binarySearch_iter(arr, target):
+    left = 0
+    right = len(arr)-1
+    while left < right:
+        mid = (left+right) // 2
+        if arr[mid] == target:
+            print("{} == {}".format(arr[mid], target))
+            return mid
+        elif arr[mid] > target:
+            print("{} > {}. will now search in {}".format(
+                arr[mid], target, arr[left:mid]))
+            right = mid
+        else:
+            print("{} < {}. will now search in {}".format(
+                arr[mid], target, arr[mid+1:right+1]))
+            left = mid+1
+    return -1
+
+
 if __name__ == "__main__":
     # generate random array
     arr = list()
@@ -127,3 +188,32 @@ if __name__ == "__main__":
     arr_copy = list(arr)
     mergeSort(arr_copy)
     print("Sorted:", arr_copy)
+
+    # quick sort test
+    print()
+    print("*** Quick Sort ***")
+    print("Original Array:", arr)
+    arr_copy = list(arr)
+    quickSort(arr_copy)
+    print("Sorted:", arr_copy)
+
+    # binary search test
+    print()
+    print("*** Binary Search (Recursive) ***")
+    target = 32
+    print("Sorted Array: {}, target = {}".format(arr_copy, target))
+    index = binarySearch_rec(arr_copy, target)
+    if index > 0:
+        print("Index of {} = {}".format(target, index))
+    else:
+        print("{} not found in array".format(target))
+
+    print()
+    print("*** Binary Search (Iterative) ***")
+    target = 97
+    print("Sorted Array: {}, target = {}".format(arr_copy, target))
+    index = binarySearch_iter(arr_copy, target)
+    if index > 0:
+        print("Index of {} = {}".format(target, index))
+    else:
+        print("{} not found in array".format(target))
