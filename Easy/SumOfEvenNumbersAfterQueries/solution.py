811c4f1dd4313006544c756d64cfe3f646929e0a
Minhyeong Joe <minhyeong.billy.joe@gmail.com>
2020-12-15 22:46:20 -0800

    improved Sum of Even Numbers after Queries (Easy)

--- Easy/SumOfEvenNumbersAfterQueries/solution.py
+++ Easy/SumOfEvenNumbersAfterQueries/solution.py
@@ -1,17 +1,25 @@
 def sumEvenAfterQueries(A, queries):
     answer = list()
+    # initial sum
+    sum = 0
+    for num in A:
+        if num % 2 == 0:
+            sum += num
     for q in queries:
-        A[q[1]] += q[0]
-        sumOfEvens = 0
-        for num in A:
-            if num % 2 == 0:
-                sumOfEvens += num
-        answer.append(sumOfEvens)
+        oldVal = A[q[1]]
+        A[q[1]] = oldVal + q[0]
+        newVal = A[q[1]]
+        if oldVal % 2 == 0:
+            sum -= oldVal
+        if newVal % 2 == 0:
+            sum += newVal
+        answer.append(sum)
+
     return answer
 
 
 # test driver
 A = [1, 2, 3, 4]
-queries = [[1,0], [-3,1], [-4,0], [2,3]]
+queries = [[1, 0], [-3, 1], [-4, 0], [2, 3]]
 
 print(sumEvenAfterQueries(A, queries))
